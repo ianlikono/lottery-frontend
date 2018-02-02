5539967b0673aede9662c98143bc2a392275e17a
ianlikono <ianlikono@gmail.com>
2018-02-02 12:13:38 +0300

    Done

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,18 @@ module.exports = {
 		'react/prop-types': 0,
 		'jsx-a11y/anchor-is-valid': 0,
 		'react/prefer-stateless-function': 0,
+		'comma-dangle': 0,
+		'no-tabs': 0,
+		'no-mixed-spaces-and-tabs': 0,
+		'react/no-did-mount-set-state': 0,
+		'react/jsx-indent': 0,
+		'jsx-a11y/label-has-for': 0
 	},
 	globals: {
-		document: 1,
+		document: 1
 	},
 	parser: 'babel-eslint',
 	env: {
-		browser: 1,
-	},
+		browser: 1
+	}
 };
